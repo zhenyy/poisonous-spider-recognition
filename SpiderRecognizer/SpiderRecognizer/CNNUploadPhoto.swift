f55a21f2c8414e4cc74e4aa69a15a3d10c03c0a0
DonghanYang <officialbennyyang@gmail.com>
2019-04-24 23:51:34 +1000

    update ios app

---
+++ SpiderRecognizer/SpiderRecognizer/CNNUploadPhoto.swift
@@ -0,0 +1,144 @@
+//
+//  UploadPhoto.swift
+//  SpiderRecognizer
+//
+//  Created by Zhenyuan Ye on 16/4/19.
+//  Copyright © 2019 DonghanYang. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import AVFoundation
+import Vision
+
+class CNNUploadPhoto: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classifier: UILabel!
+    
+    var model: VNCoreMLModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        model = try? VNCoreMLModel(for: spider_vgg().model)
+    }
+    
+    @IBAction func camera(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker, animated: true)
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CNNUploadPhoto: UIImagePickerControllerDelegate {
+    func prediciton(_ image: CVPixelBuffer) {
+        // load our CoreML model
+        
+        // run an inference with CoreML
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            
+            // grab the inference results
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            
+            // grab the highest confidence result
+            guard let Observation = results.first else { return }
+            
+            // create the label text components
+            let predclass = "\(Observation.identifier)"
+            let predconfidence = String(format: "%.02f", Observation.confidence * 100)
+            
+            // set the label text
+            DispatchQueue.main.async(execute: {
+                self.classifier.text = "\(predclass) \(predconfidence)%"
+            })
+        }
+        
+        // create a Core Video pixel buffer which is an image buffer that holds pixels in main memory
+        // Applications generating frames, compressing or decompressing video, or using Core Image
+        // can all make use of Core Video pixel buffers
+        // guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // execute the request
+        try? VNImageRequestHandler(cvPixelBuffer: image, options: [:]).perform([request])
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true)
+        classifier.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        } //1
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        // Core ML
+        
+        prediciton(pixelBuffer!)
+    }
+}
---
+++ SpiderRecognizer/SpiderRecognizer/RCNNUploadPhoto.swift
@@ -0,0 +1,227 @@
+//
+//  RCNNUploadPhoto.swift
+//  SpiderRecognizer
+//
+//  Created by Zhenyuan Ye on 22/4/19.
+//  Copyright © 2019 DonghanYang. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import AVFoundation
+import Vision
+import CoreMedia
+
+
+class RCNNUploadPhoto: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var classifier: UILabel!
+    
+    var currentBuffer: CVPixelBuffer?
+    
+    let coreMLModel = MobileNetV2_SSDLite()
+    
+    lazy var visionModel: VNCoreMLModel = {
+        do {
+            return try VNCoreMLModel(for: coreMLModel.model)
+        } catch {
+            fatalError("Failed to create VNCoreMLModel: \(error)")
+        }
+    }()
+    
+    lazy var visionRequest: VNCoreMLRequest = {
+        let request = VNCoreMLRequest(model: visionModel, completionHandler: {
+            [weak self] request, error in
+            self?.processObservations(for: request, error: error)
+        })
+        
+        // NOTE: If you use another crop/scale option, you must also change
+        // how the BoundingBoxView objects get scaled when they are drawn.
+        // Currently they assume the full input image is used.
+        request.imageCropAndScaleOption = .scaleFill
+        return request
+    }()
+
+    let maxBoundingBoxViews = 10
+    var boundingBoxViews = [BoundingBoxView]()
+    var colors: [String: UIColor] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpBoundingBoxViews()
+        setUpLayers()
+    }
+    
+    func setUpBoundingBoxViews() {
+        for _ in 0..<maxBoundingBoxViews {
+            boundingBoxViews.append(BoundingBoxView())
+        }
+        
+        // The label names are stored inside the MLModel's metadata.
+        guard let userDefined = coreMLModel.model.modelDescription.metadata[MLModelMetadataKey.creatorDefinedKey] as? [String: String],
+            let allLabels = userDefined["classes"] else {
+                fatalError("Missing metadata")
+        }
+        
+        let labels = allLabels.components(separatedBy: ",")
+        
+        // Assign random colors to the classes.
+        for label in labels {
+            colors[label] = UIColor(red: CGFloat.random(in: 0...1),
+                                    green: CGFloat.random(in: 0...1),
+                                    blue: CGFloat.random(in: 0...1),
+                                    alpha: 1)
+        }
+    }
+    
+    func setUpLayers() {
+        // Add the bounding box layers to the UI, on top of the video preview.
+        for box in self.boundingBoxViews {
+            box.addToLayer(imageView.layer)
+        }
+    }
+    
+    func predict(pixelBuffer: CVPixelBuffer) {
+        if currentBuffer == nil {
+            currentBuffer = pixelBuffer
+            let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
+            do {
+                try handler.perform([self.visionRequest])
+            } catch {
+                print("Failed to perform Vision request: \(error)")
+            }
+            currentBuffer = nil
+        }
+    }
+    
+    func processObservations(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            if let results = request.results as? [VNRecognizedObjectObservation] {
+                self.show(predictions: results)
+            } else {
+                self.show(predictions: [])
+            }
+        }
+    }
+    
+    func show(predictions: [VNRecognizedObjectObservation]) {
+        for i in 0..<boundingBoxViews.count {
+            if i < predictions.count {
+                let prediction = predictions[i]
+                
+                /*
+                 The predicted bounding box is in normalized image coordinates, with
+                 the origin in the lower-left corner.
+                 
+                 Scale the bounding box to the coordinate system of the video preview,
+                 which is as wide as the screen and has a 16:9 aspect ratio. The video
+                 preview also may be letterboxed at the top and bottom.
+                 
+                 Based on code from https://github.com/Willjay90/AppleFaceDetection
+                 
+                 NOTE: If you use a different .imageCropAndScaleOption, or a different
+                 video resolution, then you also need to change the math here!
+                 */
+                
+                let width = view.bounds.width
+                let height = width * 10 / 9 // it used to be 16 / 9 but the box is too large
+                let offsetY = (view.bounds.height - height) / 2
+                let scale = CGAffineTransform.identity.scaledBy(x: width, y: height)
+                let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -height - offsetY)
+                let rect = prediction.boundingBox.applying(scale).applying(transform)
+                
+                // The labels array is a list of VNClassificationObservation objects,
+                // with the highest scoring class first in the list.
+                let bestClass = prediction.labels[0].identifier
+                let confidence = prediction.labels[0].confidence
+                
+                // Show the bounding box.
+                let label = String(format: "%@ %.1f", bestClass, confidence * 100)
+                let color = colors[bestClass] ?? UIColor.red
+                boundingBoxViews[i].show(frame: rect, label: label, color: color)
+            } else {
+                boundingBoxViews[i].hide()
+            }
+        }
+    }
+    
+    @IBAction func camera(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        present(cameraPicker, animated: true)
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+extension RCNNUploadPhoto: UIImagePickerControllerDelegate {
+    func prediction(_ image: CVPixelBuffer) {
+        predict(pixelBuffer: image)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true)
+//        classifier.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        } //1
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        prediction(pixelBuffer!)
+    }
+}
